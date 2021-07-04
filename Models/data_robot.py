da7e12f208922eed03c9568c7ec3c524bf67386d
Adrian <36133718+AdrianN17@users.noreply.github.com>
2021-07-04 01:08:23 -0500

    0.3
    Mejora de reconocimiento de objetivos Python

--- Models/data_robot.py
+++ Models/data_robot.py
@@ -1,6 +1,7 @@
 class data_robot():
 
     def __init__(self):
+        self.tipo = ""
         self.accion = ""
         self.data = ""
         self.objetivo = ""
--- ia.py
+++ ia.py
@@ -9,6 +9,8 @@ from PyQt5 import QtGui
 
 import time
 
+from PIL import Image, ImageDraw
+
 from Models.data_decision import data_decision
 from Models.data_robot import data_robot
 
@@ -23,7 +25,18 @@ class ia:
         json_array_string = open('face_data.json', )
         self.json_array_img = json.load(json_array_string)
 
-        #print(self.json_array_img[0]["nombre"])
+        self.rostrosBD =[]
+        self.nombresBD = []
+
+
+        for data in self.json_array_img:
+
+            face = face_recognition.load_image_file(data["img"])
+            encoding = face_recognition.face_encodings(face)[0]
+            self.rostrosBD.append(encoding)
+            self.nombresBD.append(data["nombre"])
+
+
 
         self.decision = data_decision()
 
@@ -41,11 +54,18 @@ class ia:
 
             data_parse = json.loads(json_data)
 
-            self.obj.accion = data_parse["accion"]
-            self.obj.data = data_parse["data"]
-            self.obj.objetivo = data_parse["objetivo"]
+            self.obj.tipo = data_parse["tipo"]
 
-            self.aplicar_recfacial(self.obj.data)
+            if self.obj.tipo == "robotData":
+
+                self.obj.accion = data_parse["accion"]
+                self.obj.data = data_parse["data"]
+                self.obj.objetivo = data_parse["objetivo"]
+
+                self.aplicar_recfacial(self.obj.data)
+
+            elif self.obj.tipo == "reabastecido":
+                pass
 
 
 
@@ -57,23 +77,39 @@ class ia:
         image_buffer = cv2.imdecode(jpg_as_np, flags=1)
 
         face_locations = face_recognition.face_locations(image_buffer)
+        face_encodings = face_recognition.face_encodings(image_buffer, face_locations)
+
+        pil_image = Image.fromarray(jpg_as_np)
+        draw = ImageDraw.Draw(pil_image)
+
+        pil_image = Image.fromarray(image_buffer)
+        draw = ImageDraw.Draw(pil_image)
+
+        for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(self.rostrosBD, face_encoding)
+
+            name = "Desconocido"
+
+            face_distances = face_recognition.face_distance(self.rostrosBD, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = self.nombresBD[best_match_index]
+
+            # Draw a box around the face using the Pillow module
+            draw.rectangle(((left, top), (right, bottom)), outline=(0, 0, 255))
+
+            # Draw a label with a name below the face
+            text_width, text_height = draw.textsize(name)
+            draw.rectangle(((left, bottom - text_height - 10), (right, bottom)), fill=(0, 0, 255), outline=(0, 0, 255))
+            draw.text((left + 6, bottom - text_height - 5), name, fill=(255, 255, 255, 255))
 
 
-        for face_location in face_locations:
-            top, right, bottom, left = face_location
+        del draw
 
-            texto = "{} Una cara ha sido reconocida Top: {}, Left: {}, Bottom: {}, Right: {} \n".format(time.strftime("%H:%M:%S"),top, left, bottom,right)
+        open_cv_image = np.array(pil_image)
 
-
-            self.txt.insertPlainText(texto)
-
-            self.decision.cantidad_caras+= 1
-
-            #face_image = image_buffer[top:bottom, left:right]
-
-            #print(texto)
-
-        img = QtGui.QImage(image_buffer.data, image_buffer.shape[1], image_buffer.shape[0],
+        img = QtGui.QImage(open_cv_image.data, open_cv_image.shape[1], open_cv_image.shape[0],
                            QtGui.QImage.Format_RGB888).rgbSwapped()
 
         pixmap = QtGui.QPixmap.fromImage(img)
